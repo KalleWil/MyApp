8c873ab74a3a1a3487b5a4f04724a92c18c825bc
KalleWil <kalle-wil@hotmail.com>
2021-09-06 17:29:31 +0200

    Tested and implemented IsLeapYear

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -6,9 +6,73 @@ namespace MyApp.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestLeapYear_is_divisble_by_four()
         {
+            // Arrange
+            var year = 4;
 
+            // Act
+            Program program = new Program();
+            var test = program.IsLeapYear(year);
+
+            // Assert
+            Assert.True(test);
+        }
+
+        [Fact]
+        public void TestLeapYear_is_not_divisble_by_four()
+        {
+            // Arrange
+            var year = 5;
+
+            // Act
+            Program program = new Program();
+            var test = program.IsLeapYear(year);
+
+            // Assert
+            Assert.False(test);
+        }
+
+        [Fact]
+        public void TestLeapYear_is_not_divisible_by_hundred()
+        {
+            // Arrange
+            var year = 100;
+
+            // Act
+            Program program = new Program();
+            var test = program.IsLeapYear(year);
+
+            // Assert
+            Assert.False(test);
+        }
+
+        [Fact]
+        public void TestLeapYear_is_divisible_by_four_hundred()
+        {
+            // Arrange
+            var year = 400;
+
+            // Act
+            Program program = new Program();
+            var test = program.IsLeapYear(year);
+
+            // Assert
+            Assert.True(test);
+        }
+
+        [Fact]
+        public void TestLeapYear_is_divisible_by_eight_hundred_eighty_eight()
+        {
+            // Arrange
+            var year = 888;
+
+            // Act
+            Program program = new Program();
+            var test = program.IsLeapYear(year);
+
+            // Assert
+            Assert.True(test);
         }
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,11 +2,19 @@
 
 namespace MyApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public bool IsLeapYear(int year){
+            if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+            {
+                return true;
+            } 
+            return false;
         }
     }
 }
