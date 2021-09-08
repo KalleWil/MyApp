bc98ae44c09d52411ee1a72f6073981c4877babb
KalleWil <kalle-wil@hotmail.com>
2021-09-08 15:15:00 +0200

    Implemented error handling

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -7,6 +7,8 @@ namespace MyApp.Tests
     public class UnitTest1
     {
 
+        /*
+
         [Theory]
         // Arrange
         [InlineData(4, true)]
@@ -34,11 +36,53 @@ namespace MyApp.Tests
 
             // Act
             Program.Main(new string[0]);
-            var output = writer.GetStringBuilder().ToString().Trim();
+            
+            var output_notFixed = writer.GetStringBuilder().ToString().Trim();
+            var output = output_notFixed.Substring(output_notFixed.LastIndexOf("\r") + 2);
 
             // Assert
-            Assert.Contains(expected, output);
+            Assert.Equal(expected, output);
         }
+
+        */
+
+        
+        [Theory]
+        // Arrange
+        [InlineData(1, false)]
+        [InlineData(1581, false)]
+        [InlineData(1582, false)]
+        [InlineData(1584, true)]
+        [InlineData(8888, true)]
+        public void Test_LeapYear_After_1582(int year, bool expected){
+            // Act and Assert
+            Program program = new Program();
+            Assert.Equal(expected, program.IsLeapYear(year));
+        }
+
+
+        [Theory]
+        // Arrange
+        [InlineData("1584", "yay")]
+        [InlineData("a", "ERROR")]
+        [InlineData("this is a input", "ERROR")]
+        [InlineData("hello world", "ERROR")]
+        public void Test_LeapYear_UserInput(string inputYear, string expected){
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            Console.SetIn(new StringReader(inputYear));
+
+            // Act
+            Program.Main(new string[0]);
+            
+            var output_notFixed = writer.GetStringBuilder().ToString().Trim();
+            var output = output_notFixed.Substring(output_notFixed.LastIndexOf("\r") + 2);
+
+            // Assert
+            Assert.Equal(expected, output);
+        }
+
+
 
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -7,22 +7,26 @@ namespace MyApp
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter a year:");
-            var year = Convert.ToInt32(Console.ReadLine());
-
-            Program program = new Program();
-            if(program.IsLeapYear(year)){
-                Console.WriteLine("yay");
-            }
-            else
+            try
             {
-                Console.WriteLine("nay");
+                var year = Convert.ToInt32(Console.ReadLine());
+                Program program = new Program();
+                if(program.IsLeapYear(year)){
+                    Console.WriteLine("yay");
+                }
+                else
+                {
+                    Console.WriteLine("nay");
+                }
             }
-            
-
+            catch (FormatException)
+            {
+                Console.WriteLine("ERROR");
+            }
         }
 
         public bool IsLeapYear(int year){
-            if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+            if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) && year > 1581)
             {
                 return true;
             } 
