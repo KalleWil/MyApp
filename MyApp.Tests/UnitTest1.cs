3de955c0338d18abc564ccf633fc688a5751ddd9
KalleWil <kalle-wil@hotmail.com>
2021-09-08 14:38:32 +0200

    Updated unitTest so no duplicate of code appears

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -6,143 +6,38 @@ namespace MyApp.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void TestLeapYear_is_divisble_by_four()
-        {
-            // Arrange
-            var year = 4;
 
-            // Act
+        [Theory]
+        // Arrange
+        [InlineData(4, true)]
+        [InlineData(5, false)]
+        [InlineData(100, false)]
+        [InlineData(400, true)]
+        [InlineData(888, true)]
+        public void Test_LeapYear(int year, bool expected){
+            // Act and Assert
             Program program = new Program();
-            var test = program.IsLeapYear(year);
-
-            // Assert
-            Assert.True(test);
-        }
-
-        [Fact]
-        public void TestLeapYear_is_not_divisble_by_four()
-        {
-            // Arrange
-            var year = 5;
-
-            // Act
-            Program program = new Program();
-            var test = program.IsLeapYear(year);
-
-            // Assert
-            Assert.False(test);
-        }
-
-        [Fact]
-        public void TestLeapYear_is_not_divisible_by_hundred()
-        {
-            // Arrange
-            var year = 100;
-
-            // Act
-            Program program = new Program();
-            var test = program.IsLeapYear(year);
-
-            // Assert
-            Assert.False(test);
-        }
-
-        [Fact]
-        public void TestLeapYear_is_divisible_by_four_hundred()
-        {
-            // Arrange
-            var year = 400;
-
-            // Act
-            Program program = new Program();
-            var test = program.IsLeapYear(year);
-
-            // Assert
-            Assert.True(test);
-        }
-
-        [Fact]
-        public void TestLeapYear_is_divisible_by_eight_hundred_eighty_eight()
-        {
-            // Arrange
-            var year = 888;
-
-            // Act
-            Program program = new Program();
-            var test = program.IsLeapYear(year);
-
-            // Assert
-            Assert.True(test);
+            Assert.Equal(expected, program.IsLeapYear(year));
         }
 
 
-        [Fact]
-        public void TestLeapYear_user_input_is_leapyear()
-        {
-            // Arrange
+        [Theory]
+        // Arrange
+        [InlineData("4", "yay")]
+        [InlineData("5", "nay")]
+        [InlineData("800", "yay")]
+        [InlineData("999", "nay")]
+        public void Test_LeapYear_UserInput(string inputYear, string expected){
             var writer = new StringWriter();
             Console.SetOut(writer);
-            Console.SetIn(new StringReader("4"));
+            Console.SetIn(new StringReader(inputYear));
 
             // Act
             Program.Main(new string[0]);
-            
             var output = writer.GetStringBuilder().ToString().Trim();
 
             // Assert
-            Assert.Contains("yay", output);
-        }
-        
-        [Fact]
-        public void TestLeapYear_user_input_is_leapyear2()
-        {
-            // Arrange
-            var writer = new StringWriter();
-            Console.SetOut(writer);
-            Console.SetIn(new StringReader("800"));
-
-            // Act
-            Program.Main(new string[0]);
-            
-            var output = writer.GetStringBuilder().ToString().Trim();
-
-            // Assert
-            Assert.Contains("yay", output);
-        }
-
-        [Fact]
-        public void TestLeapYear_user_input_is_not_leapyear()
-        {
-            // Arrange
-            var writer = new StringWriter();
-            Console.SetOut(writer);
-            Console.SetIn(new StringReader("5"));
-
-            // Act
-            Program.Main(new string[0]);
-            
-            var output = writer.GetStringBuilder().ToString().Trim();
-
-            // Assert
-            Assert.Contains("nay", output);
-        }
-
-        [Fact]
-        public void TestLeapYear_user_input_is_not_leapyear2()
-        {
-            // Arrange
-            var writer = new StringWriter();
-            Console.SetOut(writer);
-            Console.SetIn(new StringReader("999"));
-
-            // Act
-            Program.Main(new string[0]);
-            
-            var output = writer.GetStringBuilder().ToString().Trim();
-
-            // Assert
-            Assert.Contains("nay", output);
+            Assert.Contains(expected, output);
         }
 
     }
