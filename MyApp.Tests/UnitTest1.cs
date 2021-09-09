226804d25d0a4d60c715894676a0c4fddf03628e
KalleWil <kalle-wil@hotmail.com>
2021-09-09 11:03:21 +0200

    Leap year (#2)
    * Tested and implemented IsLeapYear
    
    * Created user interface for IsLeapYear
    
    * Updated unitTest so no duplicate of code appears
    
    * Implemented error handling
    
    * Added documentation

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -1,14 +1,88 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace MyApp.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
 
+        /*
+
+        [Theory]
+        // Arrange
+        [InlineData(4, true)]
+        [InlineData(5, false)]
+        [InlineData(100, false)]
+        [InlineData(400, true)]
+        [InlineData(888, true)]
+        public void Test_LeapYear(int year, bool expected){
+            // Act and Assert
+            Program program = new Program();
+            Assert.Equal(expected, program.IsLeapYear(year));
         }
+
+
+        [Theory]
+        // Arrange
+        [InlineData("4", "yay")]
+        [InlineData("5", "nay")]
+        [InlineData("800", "yay")]
+        [InlineData("999", "nay")]
+        public void Test_LeapYear_UserInput(string inputYear, string expected){
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            Console.SetIn(new StringReader(inputYear));
+
+            // Act
+            Program.Main(new string[0]);
+            
+            var output_notFixed = writer.GetStringBuilder().ToString().Trim();
+            var output = output_notFixed.Substring(output_notFixed.LastIndexOf("\r") + 2);
+
+            // Assert
+            Assert.Equal(expected, output);
+        }
+
+        */
+
+        
+        [Theory]
+        // Arrange
+        [InlineData(1, false)]
+        [InlineData(1581, false)]
+        [InlineData(1582, false)]
+        [InlineData(1584, true)]
+        [InlineData(8888, true)]
+        public void Test_LeapYear_After_1582(int year, bool expected){
+            // Act and Assert
+            Program program = new Program();
+            Assert.Equal(expected, program.IsLeapYear(year));
+        }
+
+
+        [Theory]
+        // Arrange
+        [InlineData("1584", "yay")]
+        [InlineData("a", "ERROR")]
+        [InlineData("this is a input", "ERROR")]
+        [InlineData("hello world", "ERROR")]
+        public void Test_LeapYear_UserInput(string inputYear, string expected){
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            Console.SetIn(new StringReader(inputYear));
+
+            // Act
+            Program.Main(new string[0]);
+            
+            var output_notFixed = writer.GetStringBuilder().ToString().Trim();
+            var output = output_notFixed.Substring(output_notFixed.LastIndexOf("\r") + 2);
+
+            // Assert
+            Assert.Equal(expected, output);
+        }
+
+
+
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -4,9 +4,33 @@ namespace MyApp
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a year:");
+            try
+            {
+                var year = Convert.ToInt32(Console.ReadLine());
+                Program program = new Program();
+                if(program.IsLeapYear(year)){
+                    Console.WriteLine("yay");
+                }
+                else
+                {
+                    Console.WriteLine("nay");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+
+        public bool IsLeapYear(int year){
+            if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) && year > 1581)
+            {
+                return true;
+            } 
+            return false;
         }
     }
 }
